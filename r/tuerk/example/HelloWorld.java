55ffe6c4383bdd09b7d8739a0263602d052c7b51
RamsesTuerk <ramses.tuerk@iu-study.org>
2024-01-08 10:22:45 +0100

    pckg

---
+++ r/tuerk/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package r.tuerk.example;
+public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!");
+        System.out.println("Ich heisse Ramses");
+        System.out.println("Mein Programm kann super rechnen");
+        System.out.println("Das ergebniss von 795430 dividiert durch 4679 ist: " + 795430 / 4679);
+    }
+}
